bc94cc17104b3a1f7c3ba7f28f0441187fc03777
Eyinadepeter <itemize6@gmail.com>
2023-04-12 18:14:34 +0100

    add

--- main.js
+++ main.js
@@ -14,27 +14,30 @@ const fetchmusic = async () => {
   musics.slice(0, 100).forEach((music) => {
     console.log(music);
     const div = document.createElement("div");
-    div.classList = "flex flex-col lg:flex lg:flex-row md:flex md:flex-row";
+    div.classList =
+      "flex flex-col  lg:flex lg:flex-row md:flex md:flex-row border-[5px] border-red-900 mb-[20px]  font-mono";
 
     div.innerHTML = `
-    <img src="${music.trackMetadata.displayImageUri}"/>
-    <div  id="artist" class="pt-[40px]">
+    <img loading=lazy class="h-[250px] md:h-auto " src="${
+      music.trackMetadata.displayImageUri
+    }"/>
+    <div  class="lg:px-[25%]">
     <p class="text-[30px]"><span class="text-[30px] font-bold text-red-950">ARTIST :</span> ${music.trackMetadata.artists.map(
       (artist) => artist.name
     )}</p>
-    <p><span class="text-[30px] font-bold text-red-950">TRACK</span>:   ${
+    <p class="text-[30px]"><span class="text-[30px] font-bold text-red-950">TRACK :</span>   ${
       music.trackMetadata.trackName
     }
-    <p><span class="text-[30px] font-bold text-red-950">LABEL :</span>${music.trackMetadata.labels.map(
+    <p class="text-[30px]"><span class="text-[30px] font-bold text-red-950">LABEL :</span>${music.trackMetadata.labels.map(
       (labels) => labels.name
     )}</p>
-    <p><span class="text-[30px] font-bold text-red-950">RELEASE DATE :</span>   ${
+    <p class="text-[30px]"><span class="text-[30px] font-bold text-red-950">RELEASE DATE :</span>   ${
       music.trackMetadata.releaseDate
     }</p>
-    <p><span class="text-[30px] font-bold text-red-950">CURRENT RANK :</span> "${
+    <p class="text-[30px]"><span class="text-[30px] font-bold text-red-950">CURRENT RANK :</span> "${
       music.chartEntryData.currentRank
     }"</p>
-    <p><span class="text-[30px] font-bold text-red-950">RANK STATUS :</span> "${
+    <p class="text-[30px]"><span class="text-[30px] font-bold text-red-950">RANK STATUS :</span> "${
       music.chartEntryData.entryStatus
     }"</p>
 
